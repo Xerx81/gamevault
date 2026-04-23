b3362fa1cc16a34637ee77e0404d833401d94bf7
Xerx81 <ryuvraj832@gmail.com>
2026-04-23 07:59:07 +0530

    vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: "https://github.com/Xerx81/gamevault",
+    base: "/gamevault/",
     plugins: [react()],
     server: {
         proxy: {
